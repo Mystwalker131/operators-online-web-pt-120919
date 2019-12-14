dab691b19d913fe704a9c3b6cb99590c1d6d8e8e
Benito Galvez <mystwalker131@gmail.com>
2019-12-14 16:51:53 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
 def unsafe?(speed)
-
+#if/else statement
+#true if speed <40 && >60
+#false if not within range^
+if speed <40 || speed >60
+  return true 
+else false
+end
 end
 
 
 
 def not_safe?(speed)
-	
+
 end
 	
 
