ae024c024710f4d7ecff53e6f57b7d56ef858170
Benito Galvez <mystwalker131@gmail.com>
2019-12-14 17:24:01 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,4 +1,4 @@
-def unsafe?(speed)
+def unsafe?(speed) #first method
 #if/else statement 2nd
 #true if speed <40 && >60 3rd
 #false if not within range^ 4th , then end
@@ -10,7 +10,7 @@ end
 
 
 
-def not_safe?(speed)
+def not_safe?(speed) #second method
 speed<40||speed>60 ? true:false
 end
 	
